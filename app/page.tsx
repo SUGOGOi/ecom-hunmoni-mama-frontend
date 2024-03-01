4f6e02aff9a9040b71fcfcc73d9a601ab4ee89fe
Sumsum Gogoi <sumsumgogoi51@gmail.com>
2024-03-01 07:41:53 +0530

    1/3/24

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,11 @@
-"use client"
-import Lottie from "react-lottie";
-import loading from "../public/lottie/loading.json";
-import order_processing from "../public/lottie/order_processing.json";
+
+import style from "./page.module.scss"
 
 const page = () => {
-  const defaultOption = {
-    loop: true,
-    autoplay: true,
-    animationData: loading,
-    renderSetting: {
-      preverseAspectRatio: "xMidYMid slice"
-    }
-  };
+
   return (
-    <div>
-      <Lottie options={defaultOption} height={200}  width={200}/>
+    <div  className={style.container} >
+      loading
     </div>
   )
 }
